6bc7b3b02f10dba7bf26a7193a0335cbda6c0c06
MiniDigger <admin@minidigger.me>
2016-02-27 22:07:59 +0100

    Don't  let the test fail and inluce PowerMock (<-- we should use that)

--- src/test/java/me/MiniDigger/Foundation/handler/command/CommandTest.java
+++ src/test/java/me/MiniDigger/Foundation/handler/command/CommandTest.java
@@ -1,9 +1,14 @@
 package me.MiniDigger.Foundation.handler.command;
 
 import org.junit.FixMethodOrder;
+import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CommandTest {
 	// TODO Figure out a way to do method tests for command stuff
+	@Test
+	public void placeholder(){
+		
+	}
 }

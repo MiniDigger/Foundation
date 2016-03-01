d7440be9669afef87bfebb9d1cbc5a2aaf226a12
MiniDigger <admin@minidigger.me>
2016-03-01 22:37:16 +0100

    1.9 hype

--- src/main/java/me/MiniDigger/Foundation/FoundationMain.java
+++ src/main/java/me/MiniDigger/Foundation/FoundationMain.java
@@ -1,39 +1,36 @@
 package me.MiniDigger.Foundation;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.java.JavaPlugin;
-
 import me.MiniDigger.Foundation.handler.FoundationHandler;
 import me.MiniDigger.Foundation.handler.command.CommandHandler;
 import me.MiniDigger.Foundation.handler.config.ConfigHandler;
-import me.MiniDigger.Foundation.handler.lang.Lang;
-import me.MiniDigger.Foundation.handler.lang.LangHandler;
-import me.MiniDigger.Foundation.handler.lang.LangKey;
+import me.MiniDigger.Foundation.handler.lang.*;
 import me.MiniDigger.Foundation.handler.module.ModuleHandler;
+
+import org.bukkit.command.*;
+import org.bukkit.plugin.java.JavaPlugin;
 
 public class FoundationMain extends JavaPlugin {
 	private static FoundationMain INSTANCE;
 	private static boolean testMode;
 	private static final FoundationHandler[] handler = { LangHandler.getInstance(), CommandHandler.getInstance(), ConfigHandler.getInstance(),
 			ModuleHandler.getInstance() };
-
+			
 	@Override
 	public void onLoad() {
 		INSTANCE = this;
 	}
-
+	
 	@Override
 	public void onEnable() {
 		Lang.console(LangKey.Foundation.ENABLE, getDescription().getVersion());
 		initHandler();
 	}
-
+	
 	@Override
 	public void onDisable() {
 		disableHandler();
 	}
-
+	
 	private void initHandler() {
 		for (final FoundationHandler h : handler) {
 			h.onLoad();
@@ -42,30 +39,30 @@ public class FoundationMain extends JavaPlugin {
 			h.onEnable();
 		}
 	}
-
+	
 	private void disableHandler() {
 		for (final FoundationHandler h : handler) {
 			h.onDisable();
 		}
 	}
-
+	
 	@Override
 	public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
 		return super.onCommand(sender, command, label, args);
 	}
-
+	
 	public static FoundationMain getInstance() {
 		return INSTANCE;
 	}
-
+	
 	public static boolean isInTestMode() {
 		return testMode;
 	}
-
+	
 	public static void setTestMode(final boolean testMode) {
 		FoundationMain.testMode = testMode;
 	}
-
+	
 	public static CommandSender getTestCommandSender() {
 		return new TestCommandSender();
 	}

2866296418f7b1ee7077e6fd30e047d60d8da97f
MiniDigger <admin@minidigger.me>
2016-03-05 10:17:52 +0100

    Pls just go...

--- src/main/java/me/MiniDigger/Foundation/handler/module/ModuleHandler.java
+++ src/main/java/me/MiniDigger/Foundation/handler/module/ModuleHandler.java
@@ -2,56 +2,52 @@ package me.MiniDigger.Foundation.handler.module;
 
 import java.io.File;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import me.MiniDigger.Foundation.handler.FoundationHandler;
 import me.MiniDigger.Foundation.handler.lang.Lang;
 
 public class ModuleHandler extends FoundationHandler {
-
+	
 	private static ModuleHandler INSTANCE;
-
+	
 	private final List<Module> modules = new ArrayList<>();
-
+	
 	private final File moduleFolder = new File("src\\test\\resources\\testModuleFolder");
-
+	
 	private final Map<String, Class<?>> classes = new HashMap<String, Class<?>>();
 	private final Map<String, ModuleLoader> loaders = new LinkedHashMap<String, ModuleLoader>();
-
+	
 	@Override
 	public boolean onLoad() {
 		loadModules();
 		return true;
 	}
-
+	
 	@Override
 	public boolean onEnable() {
 		enableModules();
 		return true;
 	}
-
+	
 	@Override
 	public boolean onDisable() {
 		disableModules();
 		return true;
 	}
-
+	
 	public void enableModules() {
 		for (final Module m : modules) {
 			m.onEnable();
 		}
 	}
-
+	
 	public void disableModules() {
 		for (final Module m : modules) {
 			m.onDisable();
 		}
 	}
-
+	
 	public void loadModules() {
 		for (final File f : moduleFolder.listFiles((dir, name) -> {
 			return name.endsWith(".jar");
@@ -67,16 +63,16 @@ public class ModuleHandler extends FoundationHandler {
 			}
 		}
 	}
-
+	
 	public Class<?> getClassByName(final String name) {
 		Class<?> cachedClass = classes.get(name);
-
+		
 		if (cachedClass != null) {
 			return cachedClass;
 		} else {
 			for (final String current : loaders.keySet()) {
 				final ModuleLoader loader = loaders.get(current);
-
+				
 				try {
 					cachedClass = loader.findClass(name, false);
 				} catch (final ClassNotFoundException e) {
@@ -88,19 +84,19 @@ public class ModuleHandler extends FoundationHandler {
 		}
 		return null;
 	}
-
+	
 	public void setClass(final String name, final Class<?> clazz) {
 		if (!classes.containsKey(name)) {
 			classes.put(name, clazz);
 		}
 	}
-
+	
 	public void removeClass(final String name) {
 		@SuppressWarnings("unused")
 		Class<?> clazz = classes.remove(name);
 		clazz = null; // Bye!
 	}
-
+	
 	public static ModuleHandler getInstance() {
 		if (INSTANCE == null) {
 			INSTANCE = new ModuleHandler();

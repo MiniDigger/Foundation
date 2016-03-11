7797706ce37f3f79b7c2e0d0de4aac48e37bd444
MiniDigger <admin@minidigger.me>
2016-03-11 18:00:32 +0100

    0005-Modules-have-a-data-folder-now

--- src/main/java/me/MiniDigger/Foundation/handler/module/Module.java
+++ src/main/java/me/MiniDigger/Foundation/handler/module/Module.java
@@ -1,26 +1,34 @@
 package me.MiniDigger.Foundation.handler.module;
 
+import java.io.File;
+
+import me.MiniDigger.Foundation.FoundationMain;
+
 public class Module {
-	
+
 	private ModuleDescription description;
-	
+
 	public boolean onLoad() {
 		return true;
 	}
-	
+
 	public boolean onEnable() {
 		return true;
 	}
-	
+
 	public boolean onDisable() {
 		return true;
 	}
-	
+
 	public ModuleDescription getDescription() {
 		return description;
 	}
-	
+
 	public void setDescription(ModuleDescription description) {
 		this.description = description;
+	}
+
+	public File getDataFolder() {
+		return new File(FoundationMain.getInstance().getDataFolder(), description.name());
 	}
 }
